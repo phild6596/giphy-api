32ff60f02aca440ff949560b78405c3d76832480
phild6596 <phild6596@gmail.com>
2018-01-03 11:38:18 -0800

    <Updated JS> Updated the JS to include a new button on a new search.

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,14 +1,34 @@
 $(document).ready(function() {
     var apiKey = "KE2wjTsn5qqmmYQklvaRG7n94T7Kj1Ih";
 
-function gifButtons() {
-    var giphy = ["Los Angeles Chargers", "San Diego","Gibson Les Paul","Hell Yeah","No way","The Last Jedi","Star Wars","Party","The Godfather","NFL"];
+   
+    var giphy = ["Los Angeles Chargers", 
+    "San Diego",
+    "Gibson Les Paul",
+    "Hell Yeah",
+    "No way",
+    "The Last Jedi",
+    "Star Wars",
+    "Party",
+    "The Godfather",
+    "NFL"];
 
+function gifButtons() {
     for (var i = 0; i < giphy.length; i++) {
         var buttons = $("<button>" + giphy[i] + "</button>")
         buttons.appendTo("#gif-buttons");
-        console.log(buttons);
+        //console.log(buttons);
     } console.log(giphy);
 }gifButtons();
+
+//Adding new Gif buttons
+$("#gifSearch").on("click", function () {
+    event.preventDefault();
+    var newGif = $("#userInput").val().trim();
+    giphy.push(newGif);
+    $("#gif-buttons").empty();
+    gifButtons();
+    console.log(newGif);
+})
 
 })
