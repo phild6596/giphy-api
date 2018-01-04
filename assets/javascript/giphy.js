5f593f91219b601a74a3771e20a940c9563b52e6
phild6596 <phild6596@gmail.com>
2018-01-03 19:30:32 -0800

    <Updated JS> JS was updated for algorithm that prints API info into HTML.

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
     var apiKey = "KE2wjTsn5qqmmYQklvaRG7n94T7Kj1Ih";
-
-   
     var giphy = ["Los Angeles Chargers", 
     "San Diego",
     "Gibson Les Paul",
@@ -15,11 +13,33 @@ $(document).ready(function() {
 
 function gifButtons() {
     for (var i = 0; i < giphy.length; i++) {
-        var buttons = $("<button>" + giphy[i] + "</button>")
+        var buttons = $("<button class='searchGif' id='"+giphy[i]+"'>" + giphy[i] + "</button>")
         buttons.appendTo("#gif-buttons");
         //console.log(buttons);
     } console.log(giphy);
 }gifButtons();
+
+function searchGiphs(term) {
+    term = term.split(" ").join("%20");
+    var gifsDiv =$(giphy).attr("data-name");
+    var searchURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + term + "&limit=10&offset=0&rating=&lang=en";
+
+    $.ajax({
+        url: searchURL,
+        method: "GET"
+    }).done(function (response) {
+        for (j = 0; j < 10; j++) {
+            var rating = response.data[j].rating;
+            var urlThumb = response.data[j].images.original.url;
+            var urlStill = response.data[j].images.original_still.url;
+            var gifHolder = $("<div class='gifRow'>");
+            var img = "<img class='gifImg' data-moving='" + urlThumb + "' src='" + urlStill + "'>";
+            var rated = "<p>Rating: " + rating.toUpperCase() + "</p>";
+            gifHolder.append(rated, img);
+            $(".gif-results").append(gifHolder);
+        }
+    });
+};
 
 //Adding new Gif buttons
 $("#gifSearch").on("click", function () {
@@ -30,5 +50,11 @@ $("#gifSearch").on("click", function () {
     gifButtons();
     console.log(newGif);
 })
+
+$("#gif-buttons").on("click", ".searchGif", function () {
+    $(".gif-results").empty();
+    var searchWord = $(this).attr("id");
+    searchGiphs(searchWord);
+});
 
 })
